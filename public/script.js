1d930c239848622d18cfcf5fa36a19be49745a6c
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-10 17:05:48 +0100

    Convert HTML into JS cleaner code

--- public/script.js
+++ public/script.js
@@ -1,39 +1,71 @@
-window.onload = () => {
-	setTimeout(
-		() => navigator
-			.geolocation
-			.getCurrentPosition(
-				position => {
-					document
-						.querySelector("#info")
-						.innerHTML =
-						`${position.coords.latitude} ${position.coords.longitude}) ${position.coords.accuracy}`
+const ASSETS = [
+	{ id: 'antenna-obj', src: 'assets/antenna-uhf.obj' },
+	{ id: 'antenna-mtl', src: 'assets/antenna-mtl.obj' },
+	{ id: 'detector-obj', src: 'assets/detector-magnetico.obj' },
+	{ id: 'detector-mtl', src: 'assets/detector-magnetico.mtl' },
+	{ id: 'telegrafo-obj', src: 'assets/telegrafo.obj' },
+	{ id: 'telegrafo-mtl', src: 'assets/telegrafo.mtl' },
+]
 
-					// document
-					// 	.querySelector("#block")
-					// 	.remove()
 
-					document
-						.querySelector("#antenna")
-						.setAttribute(
-							'gps-entity-place',
-							`latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude};`
-						)
 
-					document
-						.querySelector("#detector")
-						.setAttribute(
-							'gps-entity-place',
-							`latitude: ${position.coords.latitude}; longitude: ${position.coords.longitude};`
-						)
-				},
-				() => { },
-				{
-					enableHighAccuracy: true,
-					timeout: 5000,
-					maximumAge: Infinity
-				}
-			),
-		2000
-	)
+function createElement(tag, attributes, children) {
+	let element = document.createElement(tag)
+
+	if (attributes)
+		for (const [name, value] of Object.entries(attributes))
+			element.setAttribute(name, value)
+
+	if (children)
+		element.append(children)
+
+	return element;
 }
+
+window.onload = () => {
+	// AFRAME.regi
+	let scene = document.querySelector('a-scene')
+
+	for (let value = 0; value <= 5; value++) {
+		scene.append(
+			createElement(
+				'a-marker',
+				{
+					'value': `${value}`,
+					type: 'barcode',
+					emitevent: 'true',
+				},
+				createElement(
+					'a-obj-model',
+					{
+						id: 'telegrafo',
+						src: '#telegrafo-obj',
+						mtl: '#telegrafo-mtl',
+						position: '0 0 0',
+						scale: '1 1 1',
+					}
+				)
+			)
+		)
+	}
+
+	// let assets = createElement('a-assets')
+
+	// for (asset of assets) {
+	// }
+}
+
+// createElement(
+// 	'a-entity',
+// 	{
+// 		'gltf-model': '#antenna-glb',
+// 		position: '0 0 0',
+// 		scale: '2 2 2',
+// 	}
+// )
+
+//  <a-obj-model id="telegrafo" src="#telegrafo-obj" mtl="#telegrafo-mtl" position="0 0 0" scale="1 1 1">
+// </a-obj-model>
+
+
+// {/* <a-entity gltf-model="#antenna-glb" position="0 0 0" scale="10 10 10"></a-entity>  */ }
