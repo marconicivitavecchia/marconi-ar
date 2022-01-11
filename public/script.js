19f8100d070b21d2ccd7e63ed01f2d00b1bf48d6
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-11 21:12:46 +0100

    Refactored js code & added magnemite

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+let localStorage = window.localStorage
+
 const ASSETS = [
 	{ id: 'antenna-obj', src: './assets/antenna-uhf.obj' },
 	{ id: 'antenna-mtl', src: './assets/antenna-uhf.mtl' },
@@ -7,7 +9,41 @@ const ASSETS = [
 	{ id: 'telegrafo-mtl', src: 'assets/telegrafo.mtl' },
 ]
 
-let localStorage = window.localStorage
+const markers = ['antenna', 'telegrafo', 'detector']
+
+window.onload = () => {
+	localStorage.setItem('foundMarkers', JSON.stringify([]))
+
+	let scene = document.querySelector('a-scene')
+
+	let magnemite = createElement(
+		'a-marker',
+		{
+			id: 'magnemite',
+			value: '3',
+			type: 'barcode',
+			emitevent: 'true',
+		},
+		createElement(
+			'a-entity',
+			{
+				id: 'magnemite',
+				'gltf-model': './assets/magnemite/scene.gltf',
+				position: '0 0 0',
+				scale: '0.15 0.15 0.15',
+				'animation-mixer': ''
+			}
+		)
+	)
+
+	magnemite.addEventListener('markerFound', onMarkerFound)
+
+	scene.append(
+		loadAssets(),
+		...generateMarkers(),
+		magnemite
+	)
+}
 
 function createElement(tag, attributes, ...children) {
 	let element = document.createElement(tag)
@@ -22,63 +58,62 @@ function createElement(tag, attributes, ...children) {
 	return element
 }
 
-window.onload = () => {
-	let scene = document.querySelector('a-scene')
+const onMarkerFound = (event) => {
+	let markers = new Set(JSON.parse(localStorage.getItem('foundMarkers'))) || new Set()
+	markers.add(event.target.id)
+	localStorage.setItem('foundMarkers', JSON.stringify([...markers]))
 
-	localStorage.setItem('foundMarkers', JSON.stringify([]))
+	document
+		.querySelector('#info')
+		.innerHTML = `Trovati ${markers.size} su 5`
+}
 
-	scene.append(
-		createElement(
-			'a-assets',
-			{},
-			...ASSETS
-				.map(
-					({ id, src }) =>
-						createElement(
-							'a-asset',
-							{ id: `${id}`, src: `${src}` }
-						)
-				)
-		)
+const loadAssets = () =>
+	createElement(
+		'a-assets',
+		{},
+		...ASSETS
+			.map(
+				({ id, src }) =>
+					createElement(
+						'a-asset',
+						{ id: `${id}`, src: `${src}` }
+					)
+			)
 	)
 
-	for (let value = 0; value <= 5; value++) {
-		let marker = createElement(
-			'a-marker',
-			{
-				id: `${value}`,
-				value: `${value}`,
-				type: 'barcode',
-				emitevent: 'true',
-			},
-			createElement(
-				'a-obj-model',
-				{
-					id: 'telegrafo',
-					src: '#antenna-obj',
-					mtl: '#antenna-mtl',
-					position: '0 0 0',
-					scale: '1 1 1',
-				}
-			)
-		)
 
-		marker.addEventListener(
-			'markerFound',
-			(event) => {
-				let markers = new Set(JSON.parse(localStorage.getItem('foundMarkers'))) || new Set()
-				markers.add(event.target.id)
-				localStorage.setItem('foundMarkers', JSON.stringify([...markers]))
 
-				document
-					.querySelector('#info')
-					.innerHTML = `Trovati ${markers.size} su 5`
+const generateMarkers = () =>
+	markers
+		.map(
+			(name, value) => {
+				let marker = createElement(
+					'a-marker',
+					{
+						id: `${value}`,
+						value: `${value}`,
+						type: 'barcode',
+						emitevent: 'true',
+					},
+					createElement(
+						'a-obj-model',
+						{
+							id: name,
+							src: `#${name}-obj`,
+							mtl: `#${name}-mtl`,
+							position: '0 0 0',
+							scale: '1 1 1',
+						}
+					)
+				)
+
+				marker.addEventListener('markerFound', onMarkerFound)
+
+				return marker;
 			}
 		)
 
-		scene.append(marker)
-	}
-}
 
 //  <a-obj-model id="telegrafo" src="#telegrafo-obj" mtl="#telegrafo-mtl" position="0 0 0" scale="1 1 1">
 // </a-obj-model>
