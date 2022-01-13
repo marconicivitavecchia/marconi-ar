4aa0ee36414f8ccd7cf5720e5d5c37ef13ce653d
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-01-13 21:31:02 +0100

    Refactor files

--- public/assets.js
+++ public/assets.js
@@ -1,4 +1,39 @@
-const MARKERS = ['logo', 'detector', 'antenna', 'telegrafo', 'sphere']
+const MARKERS = ['logo', 'antenna', 'detector', 'telegrafo', 'sphere', 'marconi']
+
+const descriptions = [
+	{
+		title: 'Logo festival VR',
+		description: 'Hai partecipato al festival VR? Non sai cosa ti sei perso!'
+	},
+	{
+		title: 'Antenna monopolo (1896)',
+		description: 'Riceve ed invia onde radio anche a lunghe distanze. Sono tutt’ora utilizzate dalle stazioni radio.'
+	},
+	{
+		title: 'Detector Magnetico (1902)',
+		description: 'Rilevatore di onde radio, molto utilizzato per ricevere messaggi telegrafici data la sua grande affidabilità e resistenza alle vibrazioni.'
+	},
+	{
+		title: 'Telegrafo senza fili (1894-1895)',
+		description: 'Trasmettere messaggi tramite onde radio, unico metodo di comunicazione a lunga distanza fino alla Prima guerra mondiale, dove si iniziò ad utilizzare la radio per trasmettere, tramite le frequenze AM la voce.'
+	},
+	{
+		title: 'Project Fairytale',
+		description: 'Non hai ancora giocato a Project Fairytale per Oculus?!? Vallo subito a provare!'
+	},
+	{
+		title: 'Guglielmo Marconi Civitavecchia',
+		description: 'Beh... che dire...'
+	},
+	{
+		title: '',
+		description: ''
+	},
+	{
+		title: 'Magnemite',
+		description: 'È comparso un Magnemite selvatico!'
+	}
+]
 
 function loadAssets() {
 	let assets = []
